9144170564b4188f852fbac851306fab1f5559e9
jsandov <46768834+jsandov@users.noreply.github.com>
2020-09-27 17:01:00 -0700

    Commiting GUI template code

--- src/PCCCIS234A/Main.java
+++ src/PCCCIS234A/Main.java
@@ -1,8 +1,30 @@
 package PCCCIS234A;
 
+import PCCCIS234A.ui.MainUI;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
     }
+
+    private static void createGUI() {
+        MainUI ui = new MainUI();
+        JPanel root = ui.getRootPanel();
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(root);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
---
+++ src/PCCCIS234A/ui/MainUI.java
@@ -0,0 +1,11 @@
+package PCCCIS234A.ui;
+
+import javax.swing.*;
+
+public class MainUI {
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+
+    private JPanel rootPanel;
+}
