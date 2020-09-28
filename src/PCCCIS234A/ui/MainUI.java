486d64ec6fa667ea646ab2866fa2558131b3e490
jsandov <46768834+jsandov@users.noreply.github.com>
2020-09-27 17:54:16 -0700

    Finished setting up example combo box and Model in show tabl

--- src/PCCCIS234A/ui/MainUI.java
+++ src/PCCCIS234A/ui/MainUI.java
@@ -1,17 +1,61 @@
 package PCCCIS234A.ui;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 public class MainUI {
+    private JPanel rootPanel;
+    private JTable showTable;
+    private JComboBox genreCombo;
+    private JComboBox typeCombo;
+    private JTextField minVotesField;
+    private JButton episodeButton;
+    private JButton imdbButton;
+
+    public MainUI() {
+        createTable();
+        createGenreCombo();
+        creatTypeCombo();
+    }
+
+
+
     public JPanel getRootPanel() {
         return rootPanel;
     }
 
-    private JPanel rootPanel;
-    private JTable table1;
-    private JComboBox genreCombo;
-    private JComboBox typeCombo;
-    private JTextField minVotesField;
-    private JButton episodeButtone;
-    private JButton imdbButton;
+    private void createTable() {
+        Object[][] data = {
+                {"The Dark Knight", 2008, 9.0, 1886938},
+                {"Stars Wars: Episode V - The Empire Strikes Back", 1980, 8.8, 962165},
+                {"Inception", 2010, 8.8, 1675116}};
+        showTable.setModel(new DefaultTableModel(
+                data,
+                new String[]{"Title", "Year", "Rating", "Num Votes"}
+        ));
+        TableColumnModel columns = showTable.getColumnModel();
+        columns.getColumn(0).setMinWidth(250);
+
+        DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
+        centerRender.setHorizontalAlignment(JLabel.CENTER);
+        columns.getColumn(1).setCellRenderer(centerRender);
+        columns.getColumn(2).setCellRenderer(centerRender);
+        columns.getColumn(3).setCellRenderer(centerRender);
+
+    }
+    private void creatTypeCombo() {
+        typeCombo.setModel(new DefaultComboBoxModel(new String[]{"Movie", "tvSeries", "tvEpisode", "short"}));
+
+    }
+
+    private void createGenreCombo() {
+        genreCombo.setModel(new DefaultComboBoxModel(new String[]{"Action", "Fantasy", "Drama", "Romance", "Sci-Fi"}));
+    }
+
+
+
+
 }
